b151e1e3f2d10d59603f24482ac77bdaa1712c4e
aneta-bielska <aneta.bielska@yahoo.com>
2016-03-12 16:49:54 +0100

    add assets management

--- Gemfile
+++ Gemfile
@@ -2,3 +2,10 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'rack'
+
+gem 'slim'
+gem 'sprockets'
+gem 'uglifier'
+gem 'sass'
+gem 'coffee-script'
+gem 'execjs'
--- app.rb
+++ app.rb
@@ -1,5 +1,26 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sprockets'
+require 'uglifier'
+require 'sass'
+require 'coffee-script'
+require 'execjs'
+require 'slim'
 
-get '/' do
-  "Home for paws"
+class App < Sinatra::Base
+  set :environment, Sprockets::Environment.new
+
+  environment.append_path "assets/stylesheets"
+  environment.append_path "assets/javascripts"
+
+  environment.js_compressor  = :uglify
+  environment.css_compressor = :scss
+
+  get "/assets/*" do
+    env["PATH_INFO"].sub!("/assets", "")
+    settings.environment.call(env)
+  end
+
+  get "/" do
+    slim :index
+  end
 end
